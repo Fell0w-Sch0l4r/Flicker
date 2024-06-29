304f54ee02a7125bbbcb0119647b1a75dd40bf78
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-06-29 19:14:23 +0100

    create main variables

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,3 +2,9 @@ import "../styles/tailwind.css";
 import Movie from "./interfaces/Movie";
 import APIMovie from "./interfaces/TMDBAPIMovie";
 import axios from "axios";
+
+
+const searchBar = document.querySelector("form") as HTMLFormElement;
+const searchInput = document.querySelector(".search") as HTMLInputElement;
+const mana = document.querySelector("main") as HTMLDivElement;
+const mainHeader = document.querySelector(".mainHeader") as HTMLDivElement;
