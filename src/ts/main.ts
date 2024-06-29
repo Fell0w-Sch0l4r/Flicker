9238fdb70215c20e992e349a039fedb05fb44e20
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-06-29 19:16:36 +0100

    add event to title

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,3 +8,12 @@ const searchBar = document.querySelector("form") as HTMLFormElement;
 const searchInput = document.querySelector(".search") as HTMLInputElement;
 const mana = document.querySelector("main") as HTMLDivElement;
 const mainHeader = document.querySelector(".mainHeader") as HTMLDivElement;
+
+const favoriteMovies: Movie[] = getFavoriteMoviesfromLocal();
+
+const checkbox = document.querySelector(".favoriteMovies") as HTMLInputElement;
+
+
+mainHeader.addEventListener("click", () => {
+    getPopularMovies();
+});
