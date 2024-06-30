52fc5ff33095fad95de506fe6343a09e37e03b82
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-06-30 16:16:19 +0100

    delete TMDBAxiosResponse interface

--- src/ts/interfaces/TMDBAxiosResponse.ts
+++
@@ -1,8 +0,0 @@
-import APIMovie from "./TMDBAPIMovie";
-
-export default interface TMDBAxiosResponse {
-    page: number;
-    results: APIMovie[];
-    total_pages: number;
-    total_results: number
-}
